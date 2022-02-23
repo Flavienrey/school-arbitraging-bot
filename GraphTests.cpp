0b1ba518fb8e112366bb9e32d8971a05006dc4d6
Flavien Rey <Wernete49@gmail.com>
2022-02-23 11:35:22 +0100

    Test battery fully commented

--- GraphTests.cpp
+++ GraphTests.cpp
@@ -5,8 +5,9 @@
 #include "GraphTests.hpp"
 #include "Graph.hpp"
 
+//Starts all tests and return true if the execution is successful
 bool startAllTests(){
-    bool successStatus = true;
+    bool successStatus;
 
     cout<<endl<<"[TEST] Beginning of the tests"<<endl<<endl;
     cout<<"---Testing graph constructor---"<<endl<<endl;
@@ -46,11 +47,11 @@ bool startAllTests(){
     return true;
 }
 
-
+//Tests all different cases for the constructor of the class Graph
 bool testGraph(){
     bool executionStatus;
 
-    //Should be a success
+    //Creating a graph from the file 3cryptos, should return true
     Graph newGraph =  Graph("3cryptos.txt",&executionStatus);
 
     if(executionStatus){
@@ -61,7 +62,8 @@ bool testGraph(){
         return false;
     }
 
-    newGraph =  Graph("emptyGraph.txt",&executionStatus);
+    //Creating an empty graph with 3 vertices and 0 edges, should return true
+    Graph newGraph2("emptyGraph.txt", &executionStatus);
 
     if(executionStatus){
         cout<<"[TEST] Instantiating test with an empty graph (0 edges) | VALID "<<endl;
@@ -71,8 +73,8 @@ bool testGraph(){
         return false;
     }
 
-    //Should fail to be a valid test
-    newGraph =  Graph("bob.txt",&executionStatus);
+    //Trying to create a graph from a non existing file, should fail to be a valid test
+    Graph testGraph("bob.txt", &executionStatus);
 
     if(!executionStatus){
         cout<<"[TEST] Instantiating test with a wrong filename, should get an error message | VALID "<<endl;
@@ -85,14 +87,18 @@ bool testGraph(){
     return true;
 }
 
-
+//Tests all different cases for the method setWeight of the class Graph
 bool testSetWeight(){
     bool executionStatus;
 
+    //Creates an empty graph with 3 vertices and 0 edges
     Graph newGraph =  Graph("emptyGraph.txt",&executionStatus);
 
+    //Adding an edge to the graph, should be a success
     executionStatus = newGraph.setWeight(0,1,10);
     if(executionStatus){
+
+        //We check here after the execution successfully changed the ratio
         if(newGraph.getAdjacencyMatrix()[0][1]==10){
             cout<<"[TEST] Adding a valid edge to the graph, weight changed | VALID "<<endl;
         }
@@ -105,8 +111,11 @@ bool testSetWeight(){
         return false;
     }
 
+    //Adding a new edge looping on a vertice, should not work
     executionStatus = newGraph.setWeight(1,1,10);
     if(!executionStatus){
+
+        //We check if the edge is still uninitialized
         if(newGraph.getAdjacencyMatrix()[1][1]==0){
             cout<<"[TEST] Adding an edge looping on a vertice, should drop an error | VALID "<<endl;
         }
@@ -119,8 +128,12 @@ bool testSetWeight(){
         return false;
     }
 
+    //We add an edge with a null ratio between its two vertices, should not work
     executionStatus = newGraph.setWeight(0,1,0);
+
     if(!executionStatus){
+
+        //We check if the weight of the edge is still not null
         if(newGraph.getAdjacencyMatrix()[0][1]!=0){
             cout<<"[TEST] Adding a weight of 0 on an edge, should drop an error, edge value unchanged | VALID "<<endl;
         }
@@ -133,7 +146,9 @@ bool testSetWeight(){
         return false;
     }
 
+    //We test if the first vertice refuses a negative index
     executionStatus = newGraph.setWeight(-1,1,5);
+
     if(!executionStatus){
         cout<<"[TEST] Adding an edge with a negative index on first vertice, should drop an error | VALID "<<endl;
     }
@@ -142,7 +157,9 @@ bool testSetWeight(){
         return false;
     }
 
+    //We test if the second vertice refuses a negative index
     executionStatus = newGraph.setWeight(1,-1,5);
+
     if(!executionStatus){
         cout<<"[TEST] Adding an edge with a negative index on second vertice, should drop an error | VALID "<<endl;
     }
@@ -151,7 +168,9 @@ bool testSetWeight(){
         return false;
     }
 
+    //We test if the first vertice refuses an out-of-range index
     executionStatus = newGraph.setWeight(66,1,5);
+
     if(!executionStatus){
         cout<<"[TEST] Adding an edge with an out-of-range index on the first vertice, should drop an error | VALID "<<endl;
     }
@@ -160,7 +179,9 @@ bool testSetWeight(){
         return false;
     }
 
+    //We test if the second vertice refuses an out-of-range index
     executionStatus = newGraph.setWeight(1,66,5);
+
     if(!executionStatus){
         cout<<"[TEST] Adding an edge with an out-of-range index on the second vertice, should drop an error | VALID "<<endl;
     }
@@ -172,15 +193,16 @@ bool testSetWeight(){
     return true;
 }
 
-
+//Tests all different cases for the method setTicker of the class Graph
 bool testSetTicker(){
 
     bool executionStatus;
 
     Graph newGraph =  Graph("3cryptos.txt",&executionStatus);
 
-    //Should work
+    //We check for a valid index, should work
     executionStatus = newGraph.setTicker(0,"BTC");
+
     if(executionStatus){
         cout<<"[TEST] Setting the ticker's name of index 0 | VALID "<<endl;
     }
@@ -189,7 +211,9 @@ bool testSetTicker(){
         return false;
     }
 
+    //We check for a negative index, should not work
     executionStatus = newGraph.setTicker(-1,"BTC");
+
     if(!executionStatus){
         cout<<"[TEST] Setting the ticker's name of negative index, should drop an error | VALID "<<endl;
     }
@@ -198,7 +222,9 @@ bool testSetTicker(){
         return false;
     }
 
+    //We check if it refuses as well out-of-range indexes
     executionStatus = newGraph.setTicker(66,"BTC");
+
     if(!executionStatus){
         cout<<"[TEST] Setting the ticker's name of out-of-range index, should drop an error | VALID "<<endl;
     }
